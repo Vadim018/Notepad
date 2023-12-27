a302f8822ba8c9ca86399ce63d245047df81372e
Vadim Voitsehovskiy <vadim.rolex.2005@gmail.com>
2023-12-27 16:36:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace lab_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.створитиToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.відкритиToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.зберегтиToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.вийтиToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.створитиToolStripMenuItem1,
+            this.відкритиToolStripMenuItem1,
+            this.зберегтиToolStripMenuItem1,
+            this.вийтиToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(144, 100);
+            // 
+            // створитиToolStripMenuItem1
+            // 
+            this.створитиToolStripMenuItem1.Name = "створитиToolStripMenuItem1";
+            this.створитиToolStripMenuItem1.Size = new System.Drawing.Size(143, 24);
+            this.створитиToolStripMenuItem1.Text = "Створити";
+            // 
+            // відкритиToolStripMenuItem1
+            // 
+            this.відкритиToolStripMenuItem1.Name = "відкритиToolStripMenuItem1";
+            this.відкритиToolStripMenuItem1.Size = new System.Drawing.Size(143, 24);
+            this.відкритиToolStripMenuItem1.Text = "Відкрити";
+            // 
+            // зберегтиToolStripMenuItem1
+            // 
+            this.зберегтиToolStripMenuItem1.Name = "зберегтиToolStripMenuItem1";
+            this.зберегтиToolStripMenuItem1.Size = new System.Drawing.Size(143, 24);
+            this.зберегтиToolStripMenuItem1.Text = "Зберегти";
+            // 
+            // вийтиToolStripMenuItem1
+            // 
+            this.вийтиToolStripMenuItem1.Name = "вийтиToolStripMenuItem1";
+            this.вийтиToolStripMenuItem1.Size = new System.Drawing.Size(143, 24);
+            this.вийтиToolStripMenuItem1.Text = "Вийти";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 450);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Блокнот";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem створитиToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem відкритиToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem зберегтиToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem вийтиToolStripMenuItem1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace lab_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolStrip toolStrip = new ToolStrip();
+            toolStrip.Items.Add(new ToolStripButton("Створити", null, CreateMenuItem_Click));
+            toolStrip.Items.Add(new ToolStripButton("Відкрити", null, OpenMenuItem_Click));
+            toolStrip.Items.Add(new ToolStripButton("Зберегти", null, SaveMenuItem_Click));
+            toolStrip.Items.Add(new ToolStripButton("Вихід", null, ExitMenuItem_Click));
+
+            Controls.Add(toolStrip);
+
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Вирізати", null, CutMenuItem_Click);
+            contextMenu.Items.Add("Копіювати", null, CopyMenuItem_Click);
+            contextMenu.Items.Add("Вставити", null, PasteMenuItem_Click);
+
+            MenuStrip menuStrip = new MenuStrip();
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem("Файл");
+            ToolStripMenuItem editMenu = new ToolStripMenuItem("Редагування");
+
+            fileMenu.DropDownItems.Add("Створити", null, CreateMenuItem_Click);
+            fileMenu.DropDownItems.Add("Відкрити", null, OpenMenuItem_Click);
+            fileMenu.DropDownItems.Add("Зберегти", null, SaveMenuItem_Click);
+            fileMenu.DropDownItems.Add("Вихід", null, ExitMenuItem_Click);
+
+            editMenu.DropDownItems.Add("Вирізати", null, CutMenuItem_Click);
+            editMenu.DropDownItems.Add("Копіювати", null, CopyMenuItem_Click);
+            editMenu.DropDownItems.Add("Вставити", null, PasteMenuItem_Click);
+
+            menuStrip.Items.Add(fileMenu);
+            menuStrip.Items.Add(editMenu);
+
+            Controls.Add(menuStrip);
+
+            richTextBox1.ContextMenuStrip = contextMenu;
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.FileName = "";
+        }
+
+        private void CreateMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void OpenMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void SaveMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox1.Text);
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CopyMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void PasteMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void CutMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+    }
+}
